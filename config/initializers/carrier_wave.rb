38baadb14903b8f286fce94bb4e5ad5a99659a2a
Abhinav <abhinav.srivastava@ongraph.com>
2016-12-17 23:15:45 +0530

    changes secret keys

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
 	
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => 'AKIAJUCUBR243TJNFQXQ',
-      :aws_secret_access_key  => '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',
+      :aws_access_key_id      => 'AKIAIGPZ7F65NWO6NYNA',
+      :aws_secret_access_key  => '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',
       :region                 => 'ap-southeast-1',
        path_style: true
      # :path_style => true
