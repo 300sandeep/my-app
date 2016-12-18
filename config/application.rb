96a022a12d7563757afe343e986763842bb8d1fc
Abhinav <abhinav.srivastava@ongraph.com>
2016-12-18 22:50:59 +0530

    made changes to email file

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,16 @@ module MyApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.gmail.com',
+  :domain         => 'mail.google.com',
+  :port           => 587,
+  :user_name      => '300bisht@gmail.com',
+  :password       => 'pradeeps',
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
   end
 end
