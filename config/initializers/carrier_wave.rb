6cc56fd8b41ebd5ec2a766bd219f20da9f84fff5
Abhinav <abhinav.srivastava@ongraph.com>
2016-12-18 23:39:22 +0530

    changes key of carriar wave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
 	
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => 'AKIAIGPZ7F65NWO6NYNA',
-      :aws_secret_access_key  => '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',
+      :aws_access_key_id      => 'AKIAJLD5CRM4XJ3VLNXQ',
+      :aws_secret_access_key  => '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',
       :region                 => 'ap-southeast-1',
        path_style: true
      # :path_style => true
